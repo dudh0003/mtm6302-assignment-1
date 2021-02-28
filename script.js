b00354fa0748bef0e5dcfd4615ce30a367a42944
Keval Dudhat <dudh0003@algonquinlive.com>
2021-02-28 14:51:33 -0500

    finished adding all the emoji

---
+++ script.js
@@ -0,0 +1,300 @@
+const emoji1 = document.getElementById('emoji1');
+    emoji1.innerHTML = '&#128512;';
+
+    const emoji2 = document.getElementById('emoji2');
+    emoji2.innerHTML = '&#128513;';
+
+    const emoji3 = document.getElementById('emoji3');
+    emoji3.innerHTML = '&#128514;';
+
+    const emoji4 = document.getElementById('emoji4');
+    emoji4.innerHTML = '&#128515;';
+
+    const e5 = document.getElementById('emoji5');
+    emoji5.innerHTML = '&#128516;';
+
+    const emoji6 = document.getElementById('emoji6');
+    emoji6.innerHTML = '&#128517;';
+
+    const emoji7 = document.getElementById('emoji7');
+    emoji7.innerHTML = '&#128518;';
+
+    const emoji8 = document.getElementById('emoji8');
+    emoji8.innerHTML = '&#128519;';
+
+    const emoji9 = document.getElementById('emoji9');
+    emoji9.innerHTML = '&#128520;';
+
+    const emoji10 = document.getElementById('emoji10');
+    emoji10.innerHTML = '&#128521;';
+
+    
+    const emoji11 = document.getElementById('emoji11');
+    emoji11.innerHTML = '&#128522;';
+
+    const emoji12 = document.getElementById('emoji12');
+    emoji12.innerHTML = '&#128523;';
+
+    const emoji13 = document.getElementById('emoji13');
+    emoji13.innerHTML = '&#128524;';
+
+    const emoji14 = document.getElementById('emoji14');
+    emoji14.innerHTML = '&#128525;';
+
+    const emoji15 = document.getElementById('emoji15');
+    emoji15.innerHTML = '&#128526;';
+
+    const emoji16 = document.getElementById('emoji16');
+    emoji16.innerHTML = '&#128527;';
+
+    const emoji17 = document.getElementById('emoji17');
+    emoji17.innerHTML = '&#128528;';
+
+    const emoji18 = document.getElementById('emoji18');
+    emoji18.innerHTML = '&#128529;';
+
+    const emoji19 = document.getElementById('emoji19');
+    emoji19.innerHTML = '&#128530;';
+
+    const emoji20 = document.getElementById('emoji20');
+    emoji20.innerHTML = '&#128531;';
+
+    const emoji21 = document.getElementById('emoji21');
+    emoji21.innerHTML = '&#128532;';
+
+    const emoji22 = document.getElementById('emoji22');
+    emoji22.innerHTML = '&#128533;';
+
+    const emoji23 = document.getElementById('emoji23');
+    emoji23.innerHTML = '&#128534;';
+
+    const emoji24 = document.getElementById('emoji24');
+    emoji24.innerHTML = '&#128535;';
+
+    const emoji25 = document.getElementById('emoji25');
+    emoji25.innerHTML = '&#128536;';
+
+    const emoji26 = document.getElementById('emoji26');
+    emoji26.innerHTML = '&#128537;';
+
+    const emoji27 = document.getElementById('emoji27');
+    emoji27.innerHTML = '&#128538;';
+
+    const emoji28 = document.getElementById('emoji28');
+    emoji28.innerHTML = '&#128539;';
+
+    const emoji29 = document.getElementById('emoji29');
+    emoji29.innerHTML = '&#128540;';
+
+    const emoji30 = document.getElementById('emoji30');
+    emoji30.innerHTML = '&#128541;';
+
+    const emoji31 = document.getElementById('emoji31');
+    emoji31.innerHTML = '&#128542;';
+
+    const emoji32 = document.getElementById('emoji32');
+    emoji32.innerHTML = '&#128543;';
+
+    const emoji33 = document.getElementById('emoji33');
+    emoji33.innerHTML = '&#128544;';
+
+    const emoji34 = document.getElementById('emoji34');
+    emoji34.innerHTML = '&#128545;';
+
+    const emoji35 = document.getElementById('emoji35');
+    emoji35.innerHTML = '&#128546;';
+
+    const emoji36 = document.getElementById('emoji36');
+    emoji36.innerHTML = '&#128547;';
+
+    const emoji37 = document.getElementById('emoji37');
+    emoji37.innerHTML = '&#128548;';
+
+    const emoji38 = document.getElementById('emoji38');
+    emoji38.innerHTML = '&#128549;';
+
+    const emoji39 = document.getElementById('emoji39');
+    emoji39.innerHTML = '&#128550;';
+
+    const emoji40 = document.getElementById('emoji40');
+    emoji40.innerHTML = '&#128551;';
+
+    const emoji41 = document.getElementById('emoji41');
+    emoji41.innerHTML = '&#128552;';
+
+    const emoji42 = document.getElementById('emoji42');
+    emoji42.innerHTML = '&#128553;';
+
+    const emoji43 = document.getElementById('emoji43');
+    emoji43.innerHTML = '&#128554;';
+
+    const emoji44 = document.getElementById('emoji44');
+    emoji44.innerHTML = '&#128555;';
+
+    const emoji45 = document.getElementById('emoji45');
+    emoji45.innerHTML = '&#128556;';
+
+    const emoji46 = document.getElementById('emoji46');
+    emoji46.innerHTML = '&#128557;';
+
+    const emoji47 = document.getElementById('emoji47');
+    emoji47.innerHTML = '&#128558;';
+
+    const emoji48 = document.getElementById('emoji48');
+    emoji48.innerHTML = '&#128559;';
+
+    const emoji49 = document.getElementById('emoji49');
+    emoji49.innerHTML = '&#128560;';
+
+    const emoji50 = document.getElementById('emoji50');
+    emoji50.innerHTML = '&#128561;';
+
+    const emoji51 = document.getElementById('emoji51');
+    emoji51.innerHTML = '&#128562;';
+
+    const emoji52 = document.getElementById('emoji52');
+    emoji52.innerHTML = '&#128563;';
+
+    const emoji53 = document.getElementById('emoji53');
+    emoji53.innerHTML = '&#128564;';
+
+    const emoji54 = document.getElementById('emoji54');
+    emoji54.innerHTML = '&#128565;';
+
+    const emoji55 = document.getElementById('emoji55');
+    emoji55.innerHTML = '&#128566;';
+
+    const emoji56 = document.getElementById('emoji56');
+    emoji56.innerHTML = '&#128567;';
+
+    const emoji57 = document.getElementById('emoji57');
+    emoji57.innerHTML = '&#128568;';
+
+    const emoji58 = document.getElementById('emoji58');
+    emoji58.innerHTML = '&#128569;';
+
+    const emoji59 = document.getElementById('emoji59');
+    emoji59.innerHTML = '&#128570;';
+
+    const emoji60 = document.getElementById('emoji60');
+    emoji60.innerHTML = '&#128571;';
+
+    const emoji61 = document.getElementById('emoji61');
+    emoji61.innerHTML = '&#128572;';
+
+    const emoji62 = document.getElementById('emoji62');
+    emoji62.innerHTML = '&#128573;';
+
+    const emoji63 = document.getElementById('emoji63');
+    emoji63.innerHTML = '&#128574;';
+
+    const emoji64 = document.getElementById('emoji64');
+    emoji64.innerHTML = '&#128575;';
+
+    const emoji65 = document.getElementById('emoji65');
+    emoji65.innerHTML = '&#128576;';
+
+    const emoji66 = document.getElementById('emoji66');
+    emoji66.innerHTML = '&#128577;';
+
+    const emoji67 = document.getElementById('emoji67');
+    emoji67.innerHTML = '&#128578;';
+
+    const emoji68 = document.getElementById('emoji68');
+    emoji68.innerHTML = '&#128579;';
+
+    const emoji69 = document.getElementById('emoji69');
+    emoji69.innerHTML = '&#128580;';
+
+    const emoji70 = document.getElementById('emoji70');
+    emoji70.innerHTML = '&#129296;';
+
+    const emoji71 = document.getElementById('emoji71');
+    emoji71.innerHTML = '&#129297;';
+
+    const emoji72 = document.getElementById('emoji72');
+    emoji72.innerHTML = '&#129298;';
+
+    const emoji73 = document.getElementById('emoji73');
+    emoji73.innerHTML = '&#129299;';
+
+    const emoji74 = document.getElementById('emoji74');
+    emoji74.innerHTML = '&#129300;';
+
+    const emoji75 = document.getElementById('emoji75');
+    emoji75.innerHTML = '&#129301;';
+
+    const emoji76 = document.getElementById('emoji76');
+    emoji76.innerHTML = '&#129312;';
+
+    const emoji77 = document.getElementById('emoji77');
+    emoji77.innerHTML = '&#129313;';
+
+    const emoji78 = document.getElementById('emoji78');
+    emoji78.innerHTML = '&#129314;';
+
+    const emoji79 = document.getElementById('emoji79');
+    emoji79.innerHTML = '&#129315;';
+
+    const emoji80 = document.getElementById('emoji80');
+    emoji80.innerHTML = '&#129316;';
+
+    const emoji81 = document.getElementById('emoji81');
+    emoji81.innerHTML = '&#129317;';
+
+    const emoji82 = document.getElementById('emoji82');
+    emoji82.innerHTML = '&#129318;';
+
+    const emoji83 = document.getElementById('emoji83');
+    emoji83.innerHTML = '&#129319;';
+
+    const emoji84 = document.getElementById('emoji84');
+    emoji84.innerHTML = '&#129320;';
+
+    const emoji85 = document.getElementById('emoji85');
+    emoji85.innerHTML = '&#129321;';
+
+    const emoji86 = document.getElementById('emoji86');
+    emoji86.innerHTML = '&#129322;';
+
+    const emoji87 = document.getElementById('emoji87');
+    emoji87.innerHTML = '&#129323;';
+
+    const emoji88 = document.getElementById('emoji88');
+    emoji88.innerHTML = '&#129324;';
+
+    const emoji89 = document.getElementById('emoji89');
+    emoji89.innerHTML = '&#129325;';
+
+    const emoji90 = document.getElementById('emoji90');
+    emoji90.innerHTML = '&#129327;';
+
+    const emoji91 = document.getElementById('emoji91');
+    emoji91.innerHTML = '&#129488;';
+
+    const emoji92 = document.getElementById('emoji92');
+    emoji92.innerHTML = '&#128299;';
+
+    const emoji93 = document.getElementById('emoji93');
+    emoji93.innerHTML = '&#128300;';
+
+    const emoji94 = document.getElementById('emoji94');
+    emoji94.innerHTML = '&#128301;';
+
+    const emoji95 = document.getElementById('emoji95');
+    emoji95.innerHTML = '&#128302;';
+
+    const emoji96 = document.getElementById('emoji96');
+    emoji96.innerHTML = '&#128303;';
+
+    const emoji97 = document.getElementById('emoji97');
+    emoji97.innerHTML = '&#128304;';
+
+    const emoji98 = document.getElementById('emoji98');
+    emoji98.innerHTML = '&#128305;';
+
+    const emoji99 = document.getElementById('emoji99');
+    emoji99.innerHTML = '&#128306;';
+
+    const emoji100 = document.getElementById('emoji100');
+    emoji100.innerHTML = '&#128307;';
